baeaf01e2607e0615603569198a5056aab38f9dd
Martin Chojnowski <incedofortis@gmail.com>
2018-01-28 16:16:42 +0000

    working catlog and highlight active

--- project.py
+++ project.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
+from flask import Flask, render_template, request, redirect, \
+                  jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItem
 
 
 app = Flask(__name__)
-
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db')
@@ -14,18 +14,23 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-#Show all categories
+# Show all categories
 @app.route('/')
 @app.route('/catalog/')
 def showCategories():
     catalog = session.query(Category).order_by(asc(Category.id))
-    itemsList = session.query(CategoryItem).filter_by(category_id = 1).all()
-    return render_template('catalog.html', categoryItems = itemsList, catalog = catalog)
+    itemsList = session.query(CategoryItem).filter_by(category_id=1).all()
+    return render_template('catalog.html', categoryItems=itemsList,
+                           catalog=catalog)
+
 
 @app.route('/catalog/<int:category_id>/')
 def showItems(category_id):
-    itemsList = session.query(CategoryItem).filter_by(category_id = category_id).all()
-    return render_template('catalog.html', categoryItems = itemsList)
+    catalog = session.query(Category).order_by(asc(Category.id))
+    itemsList = session.query(CategoryItem).filter_by /
+    (category_id=category_id).all()
+    return render_template('catalog.html', categoryItems=itemsList,
+                           catalog=catalog)
 
 if __name__ == '__main__':
     app.debug = True

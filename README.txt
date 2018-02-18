9129c55c05544acf4ca5de513b3ac1cf43e26b6a
Martin Chojnowski <incedofortis@gmail.com>
2018-02-18 21:07:21 +0000

    login/disconect works

--- README.txt
+++ README.txt
@@ -1,1 +1,45 @@
-Place your catalog project in this directory.
+Book Catalog is a RESTful web application using the Python framework Flask that provides a list of genre-categories that contain information about specific books. The application provides JSON endpoints.
+Only logged users are able to add/edit/delete records.
+User authentication is provided by third-party OAuth authentication by google.
+
+Installation :
+
+Python 2        https://www.python.org/download/releases/2.7/
+
+Virtual Machine
+VirtualBox		https://www.virtualbox.org/wiki/Downloads
+VirtualBox is the software that runs the VM.
+Install the *platform package* for your operating system. You do not need the extension pack or the SDK. You do not need to launch VirtualBox after installing it.
+
+**Ubuntu 14.04 Note:** If you are running Ubuntu 14.04, install VirtualBox using the Ubuntu Software Center, not the virtualbox.org web site. Due to a [reported bug](http://ubuntuforums.org/showthread.php?t=2227131), installing VirtualBox from the site may uninstall other software you need.
+
+Vagrant			https://www.vagrantup.com/downloads.html
+Vagrant is the software that configures the VM and lets you share files between your host computer and the VM's filesystem. Install the version for your operating system.
+
+**Windows Note:** The Installer may ask you to grant network permissions to Vagrant or make a firewall exception. Be sure to allow this.
+
+Fetch the Source Code and VM Configuration
+
+VM configuration files for vagrant directory:
+https://d17h27t6h515a5.cloudfront.net/topher/2017/August/59822701_fsnd-virtual-machine/fsnd-virtual-machine.zip
+
+Clone the BookCatalog from https://github.com/Mtheory/BookCatalog.git
+
+
+Running
+
+Run the virtual machine
+Using the terminal, then type **vagrant up** to launch your virtual machine.
+Now that you have Vagrant up and running type **vagrant ssh** to log into your VM.  Change to the /vagrant directory by typing **cd /vagrant**. This will take you to the shared folder between your virtual machine and host machine.
+
+Ensure that you are inside the directory that contains project.py, database_setup.py, and two directories named 'templates' and 'static'
+
+Now type **python database_setup.py** to initialize the database.
+
+Type **python populate_catalog.py** to populate the database with.
+(Optional)
+
+Run your application within the VM (python /vagrant/Book Catalog/project.py)
+Access and the application by visiting http://localhost:8000 locally
+
+Type **python project.py** to run the Flask web server. In your browser visit **http://localhost:8000** to view the BookCatalog menu app.  You should be able to view, add, edit, and delete menu items and Book Catalog when you log in.
--- project.py
+++ project.py
@@ -3,16 +3,170 @@ from flask import Flask, render_template, request, redirect, \
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItem
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+import requests
+import json
 
 
 app = Flask(__name__)
-app.secret_key = 'some_secret'
+app.secret_key = 'some_secret'###################################################
+
+
+# clientID will reference client_secrets.json file
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Book Catalog Application"
+
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+## routnig for login and create an anti forgery token: state variable.
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+
+# google login
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    print "gconect function" #######################################################
+    # confirm that the token that the client sends to the server matches the token
+    # that the server sent to the client.
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # collect the one time code from my server
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    # If an error happens along the way, then i will throw this flow exchange FlowExchangeError
+    # and send the response as JSON object.
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = credentials.access_token
+    # append this token to the following Google URL, the Google API server can
+    # verify that this is a valid token for use.
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # if my result contains any errors, then send the 500 Internal Server Error to the client.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # check if we have the right access token.
+    gplus_id = credentials.id_token['sub'] # grab the ID of the token in my credentials object
+    # compare it to the ID returned by the Google API server. If these two ID's do not match the i do not have the correct token
+    # and should return an error.
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # check to see if user is alread logged in. This will return a 200 Successful
+    # authentication without resetting all of the login session variables again.
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # in login session store credentials, Google Plus ID.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    #using Google Plus API get some more information aout user
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    # store the user datathat we are interested
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    # flas message to let the user know that they are logged in.
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+# disconnect the user from Google account
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Only disconnect a conneected user
+    access_token = login_session.get('access_token')
+    #if the acccess_token is empty we don't have record of a user, so there is no diconnect from the application
+    if access_token is None:
+        print 'Access Token is None'#return a 401 error for this case
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    # acces token and pass it into google's url for revoking
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    # now store Google's response in an object called result
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    # if response of 200 was received then i succesfully diconected fro the users
+    if result['status'] == '200':
+        # delete teh credentials .gplus_id ...etc
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        # create a response to indicate that a user successfully logged out of my app
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        #if getting other response than 200 , the somthing went worng
+        # and return a 00 message to the client with sttment of what happened
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 # JSON APIs to view Catalog/Category Information
